838a43685ec52672ec2664ec1487934c77603143
Daniel Laks <danielslakwonyero@gmail.com>
2025-11-12 10:27:27 +0300

    Database connection

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,13 @@
+<?php
+// db_connection parameters
+$host = "localhost";
+$user = "root"; // default user for xampp
+$password = ""; //Leave empty unless you have set a password
+$db = "library_db";
 
+$conn = new mysqli($host, $user, $password, $db);
+
+if ($conn->connect_error){
+    die("connection failed: " .$conn->connect_error);
+}
+?>
